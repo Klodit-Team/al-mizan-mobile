66f86fcec48219fe5239e1d9886bdeb321463408
Yams4 <ly_bahamid@esi.dz>
2026-03-13 03:30:25 +0100

    reorganisation v2

--- AlMizan/app/src/main/java/com/klodit/almizan/MainActivity.kt
+++ AlMizan/app/src/main/java/com/klodit/almizan/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.klodit.almizan.navigation.MainNavGraph
 import com.klodit.almizan.navigation.NavGraph
 import com.klodit.almizan.ui.theme.AlMizanTheme
 
@@ -13,7 +18,13 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContent {
             AlMizanTheme {
-                NavGraph()
+                var isAuthenticated by remember { mutableStateOf(false) }
+
+                if (isAuthenticated) {
+                    MainNavGraph()
+                } else {
+                    NavGraph(onAuthSuccess = { isAuthenticated = true })
+                }
             }
         }
     }
